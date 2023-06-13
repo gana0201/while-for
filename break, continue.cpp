48dbcf515623db813223fc20fbb7c0cf7041db37
gana0201 <134347364+gana0201@users.noreply.github.com>
2023-06-13 11:47:08 +0900

    Update break, continue.cpp

--- break, continue.cpp
+++ break, continue.cpp
@@ -10,3 +10,23 @@ int main(void)
 
 	return 0;
 }
+
+//p.239 프로그래밍 3번
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+	for (x = 1; x <= 7; x++) {
+		for (y = 1; y <= x; y++) {
+			printf("%d", y);
+		}
+		for (y = x + 1; y <= 7; y++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
